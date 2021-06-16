172d95bfbf8c2375a89ed162f06b06fa3cb5201e
Your Name <you@example.com>
2021-06-16 20:12:58 +0530

    aaeee

---
+++ SampleProject123/src/com/two/C.java
@@ -0,0 +1,19 @@
+package com.two;
+import com.one.*;
+
+class B extends A{
+	
+	protected void m1() {
+		
+	}
+	void display() {
+	B b=new B();
+	b.m1();
+}
+}
+public class C extends B{
+  public static void main(String[] args) {
+  
+	
+}
+}
